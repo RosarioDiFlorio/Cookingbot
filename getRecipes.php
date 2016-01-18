bd240789eecb971c4bf2c55ceef4176b26a65b56
raffaele <rafsab9@gmail.com>
2016-01-18 13:00:48 +0100

    Aggiunta prelievo ricette con ricerca words

---
+++ getRecipes.php
@@ -0,0 +1,110 @@
+<?php
+   //Controller di view
+   require_once dirname(__FILE__). '/classes/Sessione.php';
+    //Check se collegato
+    $loggedin = Sessione::isLoggedIn(true);
+    //Variabile per attivare contesto della topbar
+    $is_addrecipe = true;
+?>
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="CookingBot">
+    <meta name="author" content="CookingTeam">
+    <title>CookingBot</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Tema material design -->
+    <link href="css/material.min.css" rel="stylesheet">
+    <link href="css/material-fullpalette.min.css" rel="stylesheet">
+    <link href="css/ripples.min.css" rel="stylesheet">
+    <link href="css/roboto.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+    <!-- CSS Specifico della view-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="css/getrecipes.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php require_once("components/topbar.php"); //Inclusione topbar?>
+
+     <div class="container text-center" >
+        <div class="heading"><h2>Help us, add a new recipe!</h2></div>
+	
+	
+<div class="container text-center"  id="wordss">
+        <div class="bs-component well"   >
+    
+          
+          <h3>Insert words!</h3>
+          <div  id="words1">
+          <div class="form-group">
+            <i class="fa fa-shopping-cart fa-3x"></i>
+            <div class="col-lg-12">
+              <input type="radio" name="lang" value="en" > En
+               <input type="radio" name="lang" value="it" > It
+               
+              <input type="text" id="words">
+            </div>
+          </div>
+        </div>
+            <button type="button" class="btn btn-primary " onclick="getRecipesByWords();"><span>Search Recipes</span></button>
+       
+        </div>
+
+  </div>  
+
+
+	
+	<div class="container text-center"  id="ingredients">
+        <div class="bs-component well"   >
+		
+          <input type="hidden" value="1" id="ningredient">
+          
+          <h3>What are the ingredients of this recipe?</h3>
+          <div  id="ingredients1">
+          <div class="form-group">
+            <i class="fa fa-shopping-cart fa-3x"></i>
+              <h4>Ingredient 1</h4>
+            <div class="col-lg-12">
+              Name
+              <input type="text" id="ingredient1" class="" />
+              Quantity
+              <input type="number" id="quantity1" class="" />
+               <input type="radio" name="mis1" value="unit" onclick="show('unit','1');"> Unit
+               <input type="radio" name="mis1" value="metric" onclick="show('metric','1');"> Metric
+               <input type="radio" name="mis1" value="imperial" onclick="show('imperial','1');"> Imperial
+               <select id="misurazione1" class="" disabled>
+               </select>
+            </div>
+          </div>
+        </div>
+            <button type="button" class="btn btn-primary " onclick="add('ingredient');"><span>Add Ingredient</span></button>
+            <button type="button" class="btn btn-primary " onclick="remov('ingredient');"><span>Remove Ingredient</span></button>
+           <button type="button" class="btn btn-primary " onclick="scelte('ingredients');"><span>Continue To Add Steps</span></button>
+		   
+        </div>
+
+	</div>	
+	
+	
+   
+     <?php require_once("components/javascript-comune.php"); //Inclusione Javascript Comune ?>
+    <!-- Script specifici di view -->
+    <script src="js/getrecipes.js"></script>
+    <script src="js/lib/highcharts.js"></script>
+    <script src="js/lib/highcharts-more.js"></script>
+    <script src="http://code.highcharts.com/themes/dark-unica.js"></script>
+  </body>
+</html>
---
+++ js/getrecipes.js
@@ -0,0 +1,181 @@
+
+function getRecipesByWords(){
+    lang = $("input[name=lang]:checked").val();
+    input = $('#words').val().trim();
+    console.log(lang+input);
+
+    $.post( "API/get_recipes_API.php", { lang: lang, input: input})
+            .done(function( data ) {
+                console.log("Data Loaded: " + data );
+        });
+}
+
+
+function visibile(element) {
+	element="#"+element;
+	console.log(element);
+    $(element).prop('disabled',false);
+}
+
+function scelte(scelta) {
+    
+
+    if (scelta == 'info') {
+        console.log(scelta);
+        $('#info').hide();
+        $('#ingredients').show();
+        name = $('#name').val().trim(); 
+        numberp = $('#nopeople').val().trim();
+        cousin = $('#cusin').val().trim();
+        diet = $('#diet').val().trim();
+        occasion = $('#occasion').val().trim();  
+        course = $('#course').val().trim();
+        dati = "name: "+name+ ", numberp: "+nopeople+", cousin: "+cousin+", diet: "+diet+", occasion: "+occasion+", course:"+ course;
+        console.log(dati)
+
+        $.post( "API/insert_recipe_API.php", { name: name, numberp: numberp, cousin: cousin, diet: diet, occasion: occasion, course: course})
+            .done(function( data ) {
+                console.log("Data Loaded: " + data );
+        });
+
+
+        return;
+    }
+     
+    //Implementare altra scelta
+    if(scelta=='stages') {
+    	console.log(scelta);
+        
+        n = +$('#nstep').val() +1;
+        console.log(n);
+        dati_step ="";
+        name = $('#name').val().trim(); 
+        for (i=1;i<n;i++)
+        {   
+            step = $('#step'+i).val();
+            console.log("Step "+i+" "+step);
+             // vado a inserire il food nel caso non esiste
+            $.post( "API/insert_step_API.php", { step: step, i: i, name: name })
+            .done(function( data ) {
+                console.log("Data Loaded: " + data );
+            });
+
+        }
+
+        console.log(dati_step);
+        return;
+    }
+    
+    if(scelta=='ingredients') {
+    	console.log(scelta);
+        $('#ingredients').hide();
+        $('#stages').show();
+        // parte finale query 
+        n = +$('#ningredient').val() +1;
+        dati_ingredient ="";
+        for (i=1;i<n;i++)
+        {   
+            ingredient = $('#ingredient'+i).val().trim();
+            quantity = $('#quantity'+i).val().trim();
+            unit = $('#misurazione'+i).val().trim();
+            mis = $('input[name=mis'+i+']:checked').val().trim();
+            name = $('#name').val().trim();
+            console.log("Ricetta: "+name+" Ingredient "+i+" "+ingredient +" Quantity: "+quantity+ " Unit: "+unit+" Mis: "+mis);
+
+            // vado a inserire il food nel caso non esiste
+            $.post( "API/insert_food_API.php", { nameFood: ingredient })
+            .done(function( data ) {
+                console.log("Data Loaded: " + data );
+            });
+
+            //inserisci ingrediente con quantità
+            $.post( "API/insert_ingredient_API.php", { ingredient: ingredient, quantity: quantity, unit: unit, mis: mis, name: name, i: i })
+            .done(function( data ) {
+                console.log("Data Loaded 2: " + data );
+            });
+
+
+
+        }
+        return;
+    }
+}
+
+function add(tipo){
+    if (tipo == 'step') {
+     n = +$('#nstep').val() +1;
+     console.log(n);
+     $('#nstep').val(n);
+     stringa = "<div class='form-group'><h4>Step " +n+"</h4><div class='col-lg-12'><input type='text' id='step"+n+"' class='form-control' /></div></div>";
+     $('#steps').append(stringa);
+                        }
+    else
+    {
+    n = +$('#ningredient').val() +1;
+    console.log(n);
+    $('#ningredient').val(n);
+    stringa = "<div class='form-group'><i class="+'"fa fa-shopping-cart fa-3x"></i>'+"<h4>Ingredient "+n+"</h4><div class='col-lg-12'>Name <input type='text' id='ingredient"+n+"' /> Quantity <input type='number' id='quantity"+n+"' /> <input type='radio' name='mis"+n+"' value='unit' onclick="+'"show('+"'unit','"+n+"');"+'"'+"> Unit <input type='radio' name='mis"+n+"' value='metric' onclick="+'"'+"show('metric','"+n+"');"+'"'+"> Metric <input type='radio' name='mis"+n+"' value='imperial' onclick="+'"'+"show('imperial','"+n+"');"+'"'+"> Imperial <select id='misurazione"+n+"'' disabled></select></div></div>";
+    $('#ingredients1').append(stringa);
+    }
+}
+
+function remov(tipo){
+    if(tipo =='step') {
+    n = $('#nstep').val();
+    if(n== '1') ;
+        else{
+            n =+$('#nstep').val()-1;
+    console.log(n);
+    $('#nstep').val(n);
+    $('#steps .form-group:last-child').remove();
+            }
+                        }
+    else{
+
+        n = $('#ningredient').val();
+    if(n== '1') ;
+        else{
+            n =+$('#ningredient').val()-1;
+    console.log(n);
+    $('#ningredient').val(n);
+    $('#ingredients1 .form-group:last-child').remove();
+            }
+    }
+
+}
+
+function show(tipologia,numero) {
+
+    codice = "#misurazione"+numero;
+    console.log(tipologia+ numero);
+    $(codice).prop('disabled',false);
+    if(tipologia == 'unit') {
+        console.log(tipologia);
+        $(codice).empty().append('whatever');
+        $(codice).append('<option value="unit">Unit</option>');
+    }
+    else
+    if(tipologia == 'metric'){
+        console.log(tipologia);
+        $(codice).empty().append('whatever');
+        $(codice).append('<option value="g">g</option>');
+        $(codice).append('<option value="kl">kl</option>');
+        $(codice).append('<option value="ml">ml</option>');
+        $(codice).append('<option value="l">l</option>');
+        }
+    else
+    if(tipologia == 'imperial'){
+        console.log(tipologia);
+        $(codice).empty().append('whatever');
+        $(codice).append('<option value="tablespoon">tablespoon</option>');
+        $(codice).append('<option value="teaspoon">teaspoon</option>');
+        $(codice).append('<option value="cup">cup</option>');
+        $(codice).append('<option value="pint">pint</option>');
+        $(codice).append('<option value="ounce">ounce</option>');
+        $(codice).append('<option value="pound">pound</option>');
+        }   
+
+}
+
+
+

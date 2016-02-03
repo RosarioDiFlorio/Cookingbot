9180365047b70998cbc492fab08081280555cf96
v.venosi <v.venosi@gmail.com>
2016-02-03 15:08:18 +0100

    aggirnamento

---
+++ js/showRecipe.js
@@ -0,0 +1,28 @@
+function showSubstitutions(nome,lang)
+		{
+			//alert(" " + nome);
+			$.post( "API/get_substitution_API.php", {nameFood:nome})
+            .done(function( data ) {
+				console.log("Data Loaded: " + data );
+				 $('#header').empty();
+				$('#content').empty();
+                $('#header').append(nome);
+				$('#content').append(data);
+				$("#myModal").modal()
+				$(".rating").rating();
+				$( ".rating").on('rating.change', function(event, value, caption) {
+										console.log(value);
+										console.log(caption);
+										console.log( $( this ).attr("id"));
+										var id = $( this ).attr("id");
+										console.log(id);
+										$.post( "API/insert_vote_substitution_API.php", { nomeSub: id , voto: value  })
+											.done(function( data ) 
+											{
+														showSubstitutions(nome,lang);
+												
+											})
+									});
+        });
+			
+		}
